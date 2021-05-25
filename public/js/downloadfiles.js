12bb39e59900f3c5f4de3d8bddbfbc1ea903e4d2
Ashish Shetty <30448930+Shetty073@users.noreply.github.com>
2021-05-25 19:32:20 +0530

    cause file to open in new tab if there is only one file else download all files

--- public/js/downloadfiles.js
+++ public/js/downloadfiles.js
@@ -1,13 +1,24 @@
 // files download/view
 function downloadBills(urls) {
+    if(urls.length === 1) {
+        // open the file in new window
+        // ! TODO: Remove this block later in github version of project
+        urls.forEach(url => {
+            let a = document.createElement('a');
+            a.setAttribute('href', url);
+            a.setAttribute('target', '_blank');
+            setTimeout(a.click(), 300);
+        });
+    } else {
+        // download all files
+        urls.forEach(url => {
+            let a = document.createElement('a');
+            a.setAttribute('href', url);
+            a.setAttribute('download', '');
+            a.setAttribute('target', '_blank');
 
+            setTimeout(a.click(), 300);
+        });
+    }
 
-    urls.forEach(url => {
-        let a = document.createElement('a');
-        a.setAttribute('href', url);
-        a.setAttribute('download', '');
-        a.setAttribute('target', '_blank');
-
-        setTimeout(a.click(), 300);
-    });
 }
